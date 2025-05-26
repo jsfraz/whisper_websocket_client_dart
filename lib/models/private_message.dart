baa29964df0872499c4396b6514f0c60e2e92856
jsfraz <josefraz@seznam.cz>
2025-05-27 01:20:37 +0200

    Added RecipientOnline attribute to private message

--- lib/models/private_message.dart
+++ lib/models/private_message.dart
@@ -5,9 +5,11 @@ class PrivateMessage {
   final int senderId;
   final Uint8List message;
   final DateTime sentAt;
+  final bool recipientOnline;
 
   PrivateMessage.fromJson(Map<String, dynamic> json)
       : senderId = json['senderId'] as int,
         message = Uint8List.fromList(List<int>.from(json['message'])),
-        sentAt = DateTime.parse(json['sentAt']);
+        sentAt = DateTime.parse(json['sentAt']),
+        recipientOnline = json['recipientOnline'] as bool;
 }
