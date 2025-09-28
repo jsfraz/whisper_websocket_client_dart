07593ca3c689898fec9e18c19e3f75d7997fa9de
Josef Ráž <josefraz@seznam.cz>
2025-09-28 17:29:23 +0200

    Refactoring, added nonce and mac to messages.

--- lib/models/new_private_message.dart
+++ lib/models/new_private_message.dart
@@ -3,19 +3,27 @@ import 'dart:typed_data';
 /// A message to be sent to a private chat
 class NewPrivateMessage {
   final int receiverId;
-  final Uint8List message;
-  final Uint8List key;
+  final Uint8List content; // Encrypted content
+  final Uint8List key; // Encrypted symmetric key
+  final Uint8List nonce; // Unique nonce for encryption
+  final Uint8List mac; // Message Authentication Code for integrity
   final DateTime sentAt;
 
-  factory NewPrivateMessage(int receiverId, Uint8List message, Uint8List key) {
-    return NewPrivateMessage._internal(receiverId, message, key, DateTime.now());
+  factory NewPrivateMessage(int receiverId, Uint8List content, Uint8List key,
+      Uint8List nonce, Uint8List mac) {
+    return NewPrivateMessage._internal(
+        receiverId, content, key, nonce, mac, DateTime.now());
   }
 
-  NewPrivateMessage._internal(this.receiverId, this.message, this.key, this.sentAt);
+  NewPrivateMessage._internal(this.receiverId, this.content, this.key,
+      this.nonce, this.mac, this.sentAt);
 
   Map<String, dynamic> toJson() {
     return {
-      'message': message,
+      'content': content,
+      'key': key,
+      'nonce': nonce,
+      'mac': mac,
       'receiverId': receiverId,
       'sentAt': sentAt.toUtc().toIso8601String(),
     };
